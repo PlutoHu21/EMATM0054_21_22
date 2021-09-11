ec77a071aa67f7037d7a36949b958a460233679a
Paul O'Dowd <paul.j.odowd@gmail.com>
2021-09-11 14:52:20 +0100

    update

---
+++ 3PI_CodeStub/Labsheet_X/Labsheet_X.ino
@@ -0,0 +1,16 @@
+#include "motors.h"
+#include "linesensor.h"
+#include "encoders.h"
+#include "kinematics.h"
+#include "pid.h"
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ 3PI_CodeStub/Labsheet_X/encoders.h
@@ -0,0 +1,19 @@
+// this #ifndef stops this file
+// from being included mored than
+// once by the compiler. 
+#ifndef _ENCODERS_H
+#define _ENCODERS_H
+
+/* This header file contains template functions
+ * which you must complete to successfully use 
+ * the rotary encoders of your 3Pi+.
+ * 
+ * Some functions have a specialised form 
+ * because they will be attached to a special
+ * interrupt service within the microcontroller.
+ */
+
+
+ 
+
+#endif
